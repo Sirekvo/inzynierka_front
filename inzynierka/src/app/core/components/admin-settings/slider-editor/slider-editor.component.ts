5d3e3206b5138d5ca2f24120b1d205afec02f047
Piotr Świerczyński <piotrswierczynski50@gmail.com>
2022-11-20 16:42:38 +0100

    dodawanie sliderow

--- inzynierka/src/app/core/components/admin-settings/slider-editor/slider-editor.component.ts
+++ inzynierka/src/app/core/components/admin-settings/slider-editor/slider-editor.component.ts
@@ -31,6 +31,10 @@ export class SliderEditorComponent implements OnInit {
     task: AngularFireUploadTask;
     urlList: Array<UrlInput>;
     newPostList: Array<PostInputByTitle>;
+    isVisible= true;
+    isVisibleAddSlider= false;
+    information_to_user= '';
+    slidersCount: number;
 
     constructor(private postService: PostService,
                 private af:AngularFireStorage) { }
@@ -39,6 +43,8 @@ export class SliderEditorComponent implements OnInit {
         this.postService.getSliderUrl().subscribe(
             (url: Array<PostInput>) => {
                 this.urlList = url;
+                this.slidersCount= url.length;
+                console.log(this.slidersCount)
             },
             () => {
             }
@@ -46,6 +52,16 @@ export class SliderEditorComponent implements OnInit {
         );
 
     }
+    show_add_slider(){
+        if(this.slidersCount<3){
+            this.isVisible = false;
+            this.isVisibleAddSlider = true;
+        }
+        else{
+            this.information_to_user = 'Możesz dodać maksymalnie 3 slidery';
+        }
+    }
+
     async uploadImages() {
         console.log(this.imgSrc);
         // this.postList.map(t => t.series_id)
@@ -64,7 +80,9 @@ export class SliderEditorComponent implements OnInit {
     submit(){
         this.postService.sendSliderUrl(this.downloadUrl).subscribe(
             (resolve) =>{
-            console.log("siedzi");
+                this.isVisibleAddSlider = false;
+                this.isVisible = true;
+                this.downloadUrl = '';
         }
 
         )
@@ -80,5 +98,10 @@ export class SliderEditorComponent implements OnInit {
             this.imgSrc = "assets/images/blog/add_photo.jpg";
             this.selectedImage = null;
         }
+    }
+    back_to_start(){
+        this.isVisibleAddSlider = false;
+        this.isVisible = true;
+        this.downloadUrl = '';
     }
 }
